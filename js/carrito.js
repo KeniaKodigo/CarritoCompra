7a9455dad4bbe2bae524bc360504cd6eefecdc4c
KenyaPaiz <paizkenia5@gmail.com>
2023-02-13 20:23:55 -0600

    asignacion de variables en javascript

---
+++ js/carrito.js
@@ -0,0 +1,20 @@
+/** Asignacion de variables */
+
+const carrito = document.getElementById('carrito');
+const contenedorCarrito = document.querySelector('#lista-carrito tbody');
+const vaciarCarrito = document.getElementById('vaciar-carrito');
+const listaCursos = document.querySelector('#lista-cursos');
+
+let arregloCarrito = [];
+
+/** APARTADO DE METODOS */
+
+function cargarEventos(){
+    /**
+     * agregar un curso
+     * eliminar un curso
+     * vaciar el carrito de cursos
+     */
+}
+
+
